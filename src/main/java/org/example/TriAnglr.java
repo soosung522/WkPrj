00213c54b909813a4e6a7eaf59b5b4929c9d5c53
data8320-03 <soosumg522@gmail.com>
2024-03-21 17:01:21 +0900

    complite

--- src/main/java/org/example/TriAnglr.java
+++ src/main/java/org/example/TriAnglr.java
@@ -2,23 +2,14 @@ package org.example;
 
 public class TriAnglr {
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (i >= j) {
-                    System.out.println("*");
-                } else {
-                    System.out.println("");
+        for (int i = 1; i < 8; i++) {
+            if (i <= 4) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("*");
                 }
-            }
-            System.out.println("");
-        }
-        for (int i = 3; i > 0; i--) {
-            for (int j = 3; j > 0; j--) {
-                if (i >= j) {
-                    System.out.println("*");
-                } else {
-
-                    System.out.println("");
+            } else {
+                for (int j = 1; j <= 8 - i; j++) {
+                    System.out.print("*");
                 }
             }
             System.out.println("");
