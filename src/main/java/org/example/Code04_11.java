31fdecfe2d3289994973ac66bcdaa3cf574dfc78
data8320-03 <soosumg522@gmail.com>
2024-03-25 13:58:58 +0900

    23

---
+++ src/main/java/org/example/Code04_11.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Code04_11 {
+    public static void main(String[] args) {
+        String str = "난생처음 자바";
+        int len;
+
+        len = str.length();
+
+        System.out.println("문자열 : " + str);
+        System.out.println("문자열 길이 : " + len);
+
+
+    }
+}
