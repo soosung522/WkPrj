dc4d8f292c720105063dc66f6e88f330099490a5
data8320-03 <soosumg522@gmail.com>
2024-04-01 15:12:29 +0900

    select

---
+++ src/main/java/org/example/Code05_04.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Code05_04 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int select;
+
+        System.out.println("1~3중에 고르시오 : ");
+        select = s.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("1을 선택했습니다");
+                break;
+            case 2:
+                System.out.println("2을 선택했습니다");
+                break;
+            case 3:
+                System.out.println("3을 선택했습니다");
+                break;
+            default:
+                System.out.println("이상한 것을 선택했습니다");
+        }
+        s.close();
+    }
+}
+
