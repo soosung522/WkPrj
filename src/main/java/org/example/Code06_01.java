aef39d0934cd6fdcaeee8fd0ed3a2ddedd426304
data8320-03 <soosumg522@gmail.com>
2024-04-05 11:23:17 +0900

    반복

---
+++ src/main/java/org/example/Code06_01.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Code06_01 {
+    public static void main(String[] args) {
+        System.out.println("난생처음 자바는 재밌습니다. ^^");
+        System.out.println("난생처음 자바는 재밌습니다. ^^");
+        System.out.println("난생처음 자바는 재밌습니다. ^^");
+    }
+}
