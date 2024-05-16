c63e4fce3414001fa3bcbea1df745231d78c9332
data8320-03 <soosumg522@gmail.com>
2024-05-16 11:14:52 +0900

    wwwwq

---
+++ src/main/java/turtlle/Code08_02.java
@@ -0,0 +1,14 @@
+package turtlle;
+
+class Rabbit5 {
+    private String shape;
+    private int xPos;
+    private int yPos;
+
+    void move(int x, int y) {
+        this.xPos = x;
+        this.yPos = y;
+    }
+}
+public class Code08_02 {
+}
